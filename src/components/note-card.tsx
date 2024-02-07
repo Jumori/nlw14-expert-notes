c4df90b6ceb4f4fd050a72269a5f948b2d0d5590
Juliana Morikoshi <julianamorikoshi@gmail.com>
2024-02-07 20:55:30 -0300

    feat: apply responsive styles

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -24,12 +24,12 @@ export function NoteCard({ note }: NoteCardProps) {
 
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/60" />
-        <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+        <Dialog.Content className="fixed inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none">
           <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
             <X className="size-5" />
           </Dialog.Close>
 
-          <div className="flex flex-1 flex-col gap-3 p-5">
+          <div className="flex flex-1 flex-col gap-3 p-5 overflow-hidden">
             <span className="text-sm font-medium text-slate-300">
               {formatDistanceToNow(note.date, {
                 locale: ptBR,
@@ -37,7 +37,9 @@ export function NoteCard({ note }: NoteCardProps) {
               })}
             </span>
 
-            <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+            <div className="overflow-auto">
+              <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+            </div>
           </div>
 
           <button
